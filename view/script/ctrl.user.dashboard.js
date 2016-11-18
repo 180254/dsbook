4664f4f65cd5b86d443753e6030a215fdca63188
pooler22 <rudypawel12@gmail.com>
2016-11-18 12:55:05 +0100

    user dashboard: prepare post Notification Status Req

--- view/script/ctrl.user.dashboard.js
+++ view/script/ctrl.user.dashboard.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     "use strict";
 
     angular
@@ -15,24 +15,71 @@
     function UserDashboardCtrl($scope, $rootScope, toaster, NotificationApi) {
         $scope.data = undefined;
 
+        $scope.predefButtons = {
+            confirmation: {
+                button: "Potwierdzam",
+                msg: "Potwierdzenie odebrania wiadomości.",
+                status: "confirmed"
+            },
+        };
+
+        $scope.content = "";
+
         // recipient is user (201-1) or his room (201)
         const user = $rootScope.user.user;
-        const recipient = user + ","
-            + (user.replace(/-[0-9]+$/, "") || "-");
+        const recipient = user + "," +
+            (user.replace(/-[0-9]+$/, "") || "-");
 
         NotificationApi.main.get({
-            status: "new",
-            recipient: recipient
-        }).$promise
+                status: "new",
+                recipient: recipient
+            }).$promise
             .then((res) => {
                 $scope.data = res;
             }).catch((error) => {
-            toaster.pop({
-                type: "error",
-                title: "Błąd!",
-                body: "Nie udało się pobrać powiadomień.",
-                showCloseButton: true
+                toaster.pop({
+                    type: "error",
+                    title: "Błąd!",
+                    body: "Nie udało się pobrać powiadomień.",
+                    showCloseButton: true
+                });
             });
-        });
+
+
+
+        $scope.sendNotification = (id, status, key) => {
+            const sId = id; //??  || $scope.content;
+            const sStatus = status; // ?? || $scope.content;
+            if (!sStatus || !sId) { //!$scope.recipient ||
+                return;
+            }
+            NotificationApi.status.post({
+                    _id: sId,
+                    status: sStatus
+                }).$promise
+                .then(() => {
+                    $scope.data[key].status = sStatus;
+                    toaster.pop({
+                        type: "success",
+                        title: "Sukces!",
+                        body: "Powiadomienie zostało wysłane.",
+                        showCloseButton: true
+                    })
+                })
+                .catch(() =>
+                    toaster.pop({
+                        type: "error",
+                        title: "Porażka!",
+                        body: "Nie udało się wysłać powiadomienia.",
+                        showCloseButton: true
+                    })
+                );
+        };
+
+        $scope.sendPredefinedNotification = (id_key, key) => {
+            const predefStatus = $scope.predefButtons[key].status;
+            const predefId = $scope.data[id_key]._id;
+            $scope.sendNotification(predefId, predefStatus, id_key);
+        };
     }
 })();
