dd00a1a7930352e4e98ecf37f81c6de5c7cf57cc
pooler22 <rudypawel12@gmail.com>
2016-11-22 11:28:16 +0100

    notifications: close Notification

--- view/script/ctrl.notification.js
+++ view/script/ctrl.notification.js
@@ -31,5 +31,52 @@
                     showCloseButton: true
                 });
             });
+
+        $scope.sendCloseNotification = (id, status,id_key) => {
+            const sId = id;
+            const sStatus = status;
+            if (!sId || !sStatus) {
+                return;
+            }
+
+            NotificationApi.status.post({
+                _id: sId,
+                status: sStatus
+            }).$promise
+                .then((notification) => {
+                    $scope.data[id_key].status = sStatus;
+                    $scope.data.splice(id_key,1);
+
+                    toaster.pop({
+                        type: "success",
+                        title: "Sukces!",
+                        body: "Powiadomienie zostało zamknięte.",
+                        showCloseButton: true
+                    });
+
+                    // SmsApi.send.post({
+                    //     "notification_id": notification._id
+                    // }).$promise
+                    //     .catch((err) => console.log(err));
+
+                })
+                .catch((err) => {
+                    console.log(err);
+
+                    toaster.pop({
+                        type: "error",
+                        title: "Porażka!",
+                        body: "Nie udało się zamknąć powiadomienia.",
+                        showCloseButton: true
+                    });
+                });
+        };
+
+
+        $scope.closeNotification = (id_key) => {
+            const predefId = $scope.data[id_key]._id;
+            console.log(predefId);
+            $scope.sendCloseNotification(predefId, "closed",id_key);
+        };
     }
 })();
